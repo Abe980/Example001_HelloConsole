9fc899406c39b09c80ca4ad8205a31f7f0464091
Abe <ai_980@inbox.ru>
2023-02-09 21:45:35 +0300

    con

---
+++ Example005_Con/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("hello MASHA ura!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
